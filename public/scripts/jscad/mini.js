b5358446da5e94a2e4355881320a49f43ee246e7
toyoharu nishikawa <toyohal@email.com>
2018-04-08 21:07:54 +0900

    mini jscad mousemove

--- public/scripts/jscad/mini.js
+++ public/scripts/jscad/mini.js
@@ -4,6 +4,7 @@ export const jscad = {
   setup: function(element, width,height){
     setDOM(element)
     setCSS(width, height)
+    const coordinateDOM = document.querySelector("#minijscad-footer > small:nth-child(2)")
     const elWidth = document.getElementById("minijscad-main").getBoundingClientRect().width
     const elHeight = document.getElementById("minijscad-main").getBoundingClientRect().height
 
@@ -31,9 +32,19 @@ export const jscad = {
       .attr("stroke-dasharray","5 5");
     
     draw.screen=draw.group();
-    draw.screen.stroke({color:"black",opacity: 1.0,width:1})
+    draw.screen
+      .stroke({color:"black",opacity: 1.0,width:1.5})
     draw.screen.sheet = []
     draw.screen.sheet.push(draw.screen.group())
+
+    draw.mousemove(function(e){
+      const point = this.point()
+      const coord = {
+        x: point.x+e.clientX/draw.zoom(),
+        y: point.y-e.clientY/draw.zoom()
+      }
+      coordinateDOM.textContent=` x: ${(coord.x*100+0.5|0)/100}, y:${(coord.y*100+0.5|0)/100}`
+    })
 
     return draw 
   }
@@ -50,16 +61,22 @@ const setDOM = (element)=>{
  const liA = document.createElement("li")
  const liS = document.createElement("li")
  const minijscadMain = document.createElement("div")
+ const minijscadFooter = document.createElement("div")
+ const minijscadCopyright = document.createElement("small")
+ const minijscadCoordinate = document.createElement("small")
 
  minijscadFrame.id = "minijscad-frame"
  minijscadTitle.id = "minijscad-title"
  minijscadSidebar.id = "minijscad-sidebar"
  minijscadMain.id = "minijscad-main"
+ minijscadFooter.id = "minijscad-footer"
 
  minijscadTitle.textContent= "mini jscad"
  liM.textContent= "M"
  liA.textContent= "A"
  liS.textContent= "S"
+
+ minijscadCoordinate.textContent = "x: 0.00 ,y: 0.00"
 
  ul.appendChild(liM)
  ul.appendChild(liA)
@@ -68,6 +85,9 @@ const setDOM = (element)=>{
  minijscadFrame.appendChild(minijscadTitle)
  minijscadFrame.appendChild(minijscadSidebar)
  minijscadFrame.appendChild(minijscadMain)
+ minijscadFooter.appendChild(minijscadCopyright)
+ minijscadFooter.appendChild(minijscadCoordinate)
+ minijscadFrame.appendChild(minijscadFooter)
  elementDOM.appendChild(minijscadFrame)
 
 }
@@ -84,7 +104,7 @@ const setCSS = (width ,height)=>{
       height: ${height}px;
       display: grid;
       grid-template-columns: 100px ${width-100}px;
-      grid-template-rows: 20px ${height-20}px;
+      grid-template-rows: 20px ${height-40}px 20px;
       outline: 2px solid #708090;
       padding: 0;
       margin: 0;
@@ -165,5 +185,27 @@ const setCSS = (width ,height)=>{
       margin: 0;
     }`,style.sheet.cssRules.length)
 
-console.log(style.sheet)
+    style.sheet.insertRule(`
+    #minijscad-footer {
+      grid-row: 3 / 4;
+      grid-column: 1 / 3;
+      padding: 0;
+      margin: 0;
+      border-top: 1px solid #708090;
+      background: #d9dfe1;
+      display: flex;
+    }`,style.sheet.cssRules.length)
+
+    style.sheet.insertRule(`
+    #minijscad-footer > small:first-child {
+      flex: 0 0 65%; 
+      text-align: center;
+      border-right: 1px solid #708090;
+    }`,style.sheet.cssRules.length)
+
+    style.sheet.insertRule(`
+    #minijscad-footer > small:nth-child(2) {
+      flex: 1 0 auto; 
+      padding: 0 0 0 10px;
+    }`,style.sheet.cssRules.length)
 }
