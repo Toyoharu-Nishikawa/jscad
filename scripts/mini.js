84db54dfcc02ee2cdf234069e7abbcf9f24c9601
toyoharu nishikawa <toyohal@email.com>
2018-04-08 21:07:54 +0900

    mini jscad mousemove

--- scripts/mini.js
+++ scripts/mini.js
@@ -5,6 +5,17 @@ const draw = jscad.setup("minijscad-test", 500, 300)
 draw.screen.sheet[0].rect(100,100).move(50,50)
   .fill("none")
   .attr("vector-effect", "non-scaling-stroke")
+
 draw.screen.sheet[0].line(0,0,100,100)
   .attr("vector-effect", "non-scaling-stroke")
- 
+  .mouseover(function(){
+    this.stroke({color:"blue"}) 
+  })
+  .mouseout(function(){
+    this.stroke({color:"black"}) 
+  })
+  .click(function(){
+    this.stroke({color:"red"}) 
+  })
+
+
