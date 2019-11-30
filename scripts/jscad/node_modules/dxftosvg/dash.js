0738ce211f45244bfd1221b3b9f5c0dda2864d75
toyoharu nishikawa <toyohal@email.com>
2019-11-30 22:28:09 +0900

    update dxftosvg

---
+++ scripts/jscad/node_modules/dxftosvg/dash.js
@@ -0,0 +1,25 @@
+"use strict"
+
+export const data = [ 
+  ["CONTINUOUS", ""],  
+  ["CENTER", "80 10 10 10"],  
+  ["CENTERX2", "160 10 10 10"],  
+  ["CENTERX2", "40 10 10 10"],  
+  ["DASHED", "40 10"],  
+  ["DASHEDX2", "80 10 10 10"],  
+  ["DASHED2", "20 10 10 10"],  
+  ["PHANTOM", "80 20 20 20 20 20"],  
+  ["PHANTOMX2", "160 20 20 20 20 20"],  
+  ["PHANTOM2", "40 10 10 10 10 10"],  
+  ["DASHDOT", "40 10 2 10 "],  
+  ["DASHDOTX2", "80 10 2 10"],  
+  ["DASHDOT2", "20 10 2 10 "],  
+  ["DOT", "2 10"],  
+  ["DOTX2", "2 20"],  
+  ["DOT2", "2 10"],  
+  ["DIVIDE", "40 10 2 10 2 10"],  
+  ["DIVIDEX2", "80 10 2 10 2 10"],  
+  ["DIVIDE2", "20 10 2 10 2 10"],  
+]
+
+export const dataMap = new Map(data)
